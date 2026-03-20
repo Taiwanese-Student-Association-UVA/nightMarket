0e58000cf3ed490d7b0a102ecfa3b02e106a6880
Hannah Duong <hannah.n.duong@gmail.com>
2026-03-19 21:56:42 -0400

    added stamp cards to menu

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import stampCard from "../../assets/StampCard.svg";
+import stamps from "../../assets/stamp.svg";
+import backStamp from "../../assets/back-button.png";
 
 export default function Menu() {
   const navigate = useNavigate();
@@ -20,42 +22,6 @@ export default function Menu() {
           width: "100vw",
         }}
       >
-        {/* Stamp-style Back Button */}
-        <button
-        onClick={() => navigate(-1)}
-        style={{
-            position: "absolute",
-            top: "6%",
-            right: "5%",
-
-            width: "12%",
-            aspectRatio: "1",
-            boxSizing: "border-box",
-
-            borderRadius: "50%",
-            border: "0.45vw dashed #7A1A1A",
-            background: "#d9414100",
-            color: "#7A1A1A",
-
-            fontSize: "1.8vw",
-            fontWeight: 700,
-            letterSpacing: "0.08em",
-
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-
-            transform: "rotate(20deg)",
-            zIndex: 10,
-            cursor: "pointer",
-            padding: 0,
-            lineHeight: 1,
-        }}
-        >
-        BACK
-        </button>
-
-        {/* Stamp Card */}
         <img
           src={stampCard}
           alt="Stamp Card"
@@ -63,6 +29,40 @@ export default function Menu() {
             width: "100vw",
             height: "auto",
             display: "block",
+          }}
+        />
+
+        <img
+        src={stamps}
+        alt="Stamps"
+        style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+
+            width: "100%",
+            height: "auto",
+
+            transform: "translate(-50%, -42%) scale(0.9)",  // 👈 KEY
+            transformOrigin: "center",
+
+            zIndex: 10,
+            pointerEvents: "none",
+        }}
+        />
+        <img
+          src={backStamp}
+          alt="Back"
+          onClick={() => navigate(-1)}
+          draggable={false}
+          style={{
+            position: "absolute",
+            top: "6%",
+            right: "5%",
+            width: "12%",
+            height: "auto",
+            zIndex: 20,
+            cursor: "pointer",
           }}
         />
       </div>
