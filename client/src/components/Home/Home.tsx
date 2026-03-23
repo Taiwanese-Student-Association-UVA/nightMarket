d7626d5b26377ed1727bc9c89cfbdc277cc4c922
Hannah Duong <hannah.n.duong@gmail.com>
2026-03-22 23:50:37 -0400

    updated ui

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -7,29 +7,38 @@ export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <div style={{ width: "100vw", background: "white", padding: "4vh 4vw", boxSizing: "border-box" }}>
+    <div style={{ width: "100vw", background: "white" }}>
+      <div
+        style={{
+          position: "sticky",
+          top: "0vh",
+          zIndex: 20,
+          pointerEvents: "none",
+        }}
+      >
+        <img
+          src={headerImage}
+          alt="Night Market Header"
+          style={{
+            width: "100%",
+            height: "auto",
+            display: "block",
+          }}
+        />
+      </div>
       <div
         style={{
           width: "100%",
           position: "relative",
+          padding: "0 4vw 4vh",
+          boxSizing: "border-box",
+          marginTop: "-8vh",
         }}
       >
         <img
           src={panelsImage}
           alt="Night Market Home"
           style={{ width: "100%", height: "auto", display: "block" }}
-        />
-        <img
-          src={headerImage}
-          alt="Night Market Header"
-          style={{
-            position: "absolute",
-            left: "0%",
-            top: "0%",
-            width: "100%",
-            height: "auto",
-            pointerEvents: "none",
-          }}
         />
         <img
           src={wordsImage}
@@ -39,7 +48,7 @@ export default function Home() {
             left: "50%",
             top: "50%",
             transform: "translate(-50%, -50%)",
-            width: "94%",
+            width: "87%",
             height: "auto",
             pointerEvents: "none",
           }}
@@ -52,7 +61,7 @@ export default function Home() {
         <button
           aria-label="Menu"
           onClick={() => navigate("/menu")}
-          style={{ position: "absolute", top: "42%", left: "4%", width: "62%", height: "16%", opacity: 0, cursor: "pointer" }}
+          style={{ position: "absolute", top: "42%", left: "10%", width: "62%", height: "16%", opacity: 0, cursor: "pointer" }}
         />
         <button
           aria-label="Map"
