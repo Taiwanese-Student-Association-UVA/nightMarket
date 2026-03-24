10d91aeaa50772faaabc7bca965f8ceb4acd7068
Hannah Duong <hannah.n.duong@gmail.com>
2026-03-24 02:10:07 -0400

    fixed header in home page

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -7,13 +7,19 @@ export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <div style={{ width: "100vw", background: "white" }}>
+    <div
+      style={{
+        width: "100%",
+        background: "white",
+        overflowX: "hidden",
+      }}
+    >
+      {/* HEADER */}
       <div
         style={{
-          position: "sticky",
-          top: "0vh",
-          zIndex: 20,
-          pointerEvents: "none",
+          position: "relative",
+          width: "100%",
+          background: "transparent",
         }}
       >
         <img
@@ -23,23 +29,32 @@ export default function Home() {
             width: "100%",
             height: "auto",
             display: "block",
+            position: "relative",
+            zIndex: 10,
           }}
         />
       </div>
+
+      {/* PANELS */}
       <div
         style={{
           width: "100%",
           position: "relative",
-          padding: "0 4vw 4vh",
+          padding: "60px 4vw 4vh",
           boxSizing: "border-box",
-          marginTop: "-8vh",
+          marginTop: "-50%", // 🔥 controls overlap (adjust this)
         }}
       >
         <img
           src={panelsImage}
           alt="Night Market Home"
-          style={{ width: "100%", height: "auto", display: "block" }}
+          style={{
+            width: "100%",
+            height: "auto",
+            display: "block",
+          }}
         />
+
         <img
           src={wordsImage}
           alt="Panel Labels"
@@ -47,45 +62,77 @@ export default function Home() {
             position: "absolute",
             left: "50%",
             top: "50%",
-            transform: "translate(-50%, -50%)",
+            transform: "translate(-50%, -46%)",
             width: "87%",
             height: "auto",
             pointerEvents: "none",
           }}
         />
-        <button
-          aria-label="Activity"
-          onClick={() => navigate("/activity")}
-          style={{ position: "absolute", top: "5%", left: "4%", width: "92%", height: "9%", opacity: 0, cursor: "pointer" }}
-        />
-        <button
-          aria-label="Menu"
-          onClick={() => navigate("/menu")}
-          style={{ position: "absolute", top: "42%", left: "10%", width: "62%", height: "16%", opacity: 0, cursor: "pointer" }}
-        />
-        <button
-          aria-label="Map"
-          onClick={() => navigate("/map")}
-          style={{ position: "absolute", top: "45%", left: "58%", width: "38%", height: "21%", opacity: 0, cursor: "pointer" }}
-        />
+
         <button
           aria-label="Schedule"
-          onClick={() => navigate("/schedule")}
-          style={{ position: "absolute", top: "57%", left: "4%", width: "60%", height: "14%", opacity: 0, cursor: "pointer" }}
-        />
-        <button
-          aria-label="Directions"
-          onClick={() => {
-            window.location.href = "https://maps.app.goo.gl/ZwNFrEBbVZn1Pw1u8";
+          onClick={() => navigate("/Schedule")}
+          style={{
+            position: "absolute",
+            top: "5%",
+            left: "4%",
+            width: "92%",
+            height: "9%",
+            opacity: 0,
+            cursor: "pointer",
+            border: "none",
+            background: "transparent",
           }}
-          style={{ position: "absolute", top: "79%", left: "4%", width: "92%", height: "9%", opacity: 0, cursor: "pointer" }}
         />
+
         <button
-          aria-label="TSA Website"
+          aria-label="Activity Right"
+          onClick={() => navigate("/Activity")}
+          style={{
+            position: "absolute",
+            top: "45%",
+            left: "58%",
+            width: "38%",
+            height: "21%",
+            opacity: 0,
+            cursor: "pointer",
+            border: "none",
+            background: "transparent",
+          }}
+        />
+
+        <button
+          aria-label="Activity Left"
+          onClick={() => navigate("/Activity")}
+          style={{
+            position: "absolute",
+            top: "42%",
+            left: "4%",
+            width: "62%",
+            height: "16%",
+            opacity: 0,
+            cursor: "pointer",
+            border: "none",
+            background: "transparent",
+          }}
+        />
+
+        <button
+          aria-label="Other Information"
           onClick={() => {
             window.location.href = "https://tsaatuva.org";
           }}
-          style={{ position: "absolute", top: "90%", left: "4%", width: "92%", height: "9%", opacity: 0, cursor: "pointer" }}
+          style={{
+            position: "absolute",
+            top: "79%",
+            left: "4%",
+            width: "92%",
+            height: "9%",
+            opacity: 0,
+            cursor: "pointer",
+            border: "none",
+            background: "transparent",
+          }}
         />
       </div>
     </div>
